8952111387ea0bcedb985196659cfbb4c44c0a0a
ppepin34 <ppepin34@gmail.com>
2022-07-16 16:41:55 -0400

    added brand statement and portrait to about, added font awesome itegration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Nav from './components/Nav';
 import About from './components/About';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Portfolio></Portfolio>
         <About></About>
       </main>
     </div>
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import portrait from "../../assets/portrait.jpg";
 
 function About() {
     return(
         <section>
             <h1 id="about">Who am I?</h1>
+            <img src={portrait} />
+            <div>
+                <p>
+                Full stack web developer with a background in history and education from Virginia Commonwealth University. Recently earned a certificate in full stack web development through the University of Richmond and Trilogy Education, developing skills with JavaScript, CSS, Express.JS, Node.JS, and responsive web design. Possessed with a passion for creating applications that focus on solving real problems for users with intuitive UI. Also equipped with management, scheduling, and creative experience. 
+                </p>
+            </div>
         </section>
     )
 }
