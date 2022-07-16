0cfad5a5f585fd45bcf93515108b8b64a4b7fb92
ppepin34 <ppepin34@gmail.com>
2022-07-16 14:00:19 -0400

    first iteratin of about, start of nav

---
+++ src/components/Nav/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <header>
+            <h2>
+                <a href="/">
+                    Phil Pepin
+                </a>
+            </h2>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#about">
+                            About Me
+                        </a>
+                    </li>
+                    <li>
+                        <span>Contact</span>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Nav;
