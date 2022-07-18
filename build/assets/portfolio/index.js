d8b02fa7f6da3ae46464f4b1ac056aad858b65be
ppepin34 <ppepin34@gmail.com>
2022-07-17 23:46:31 -0400

    remove build folder from .gitignore

---
+++ build/assets/portfolio/index.js
@@ -0,0 +1,6 @@
+import imageOne from './fire-dept.jpg';
+import imageTwo from './coding-quiz.jpg';
+import imageThree from './weather-dashboard.jpg';
+import imageFour from './work-day-scheduler.jpg';
+
+export { imageOne, imageTwo, imageThree, imageFour};
